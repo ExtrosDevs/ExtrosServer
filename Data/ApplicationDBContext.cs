317686e7182ffcf400dadcd73744c8c6ef8f6d86
ahmadeleiwa <ahmadilawa@gmai.com>
2024-08-28 15:07:04 +0300

    Spcifiy the relations

--- Data/ApplicationDBContext.cs
+++ Data/ApplicationDBContext.cs
@@ -8,6 +8,24 @@ public class ApplicationDBContext: DbContext{
     public  ApplicationDBContext(DbContextOptions<ApplicationDBContext>  options):base(options){
 
     }
+      protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Configure many-to-many relationship
+        modelBuilder.Entity<PostTag>()
+            .HasKey(pt => new { pt.PostID, pt.TagValue });
+
+        modelBuilder.Entity<PostTag>()
+            .HasOne(pt => pt.Post)
+            .WithMany(p => p.PostTags)
+            .HasForeignKey(pt => pt.PostID);
+
+        modelBuilder.Entity<PostTag>()
+            .HasOne(pt => pt.Tag)
+            .WithMany(t => t.PostTags)
+            .HasForeignKey(pt => pt.TagValue);
+    }
     public DbSet<User> Users{get; set;}
     public DbSet<Comment> Comments{get;}
     public DbSet<Article> Articles{get;}
@@ -16,6 +34,8 @@ public class ApplicationDBContext: DbContext{
     public DbSet<Like> Likes{get;}
     public DbSet<Post> Posts{get; set;}
     public DbSet<Tag> Tags{get;}
+    public DbSet<PostTag> PostTags{get;}
+
     public DbSet<UserFollow> UserFollows{get;}
  
     
